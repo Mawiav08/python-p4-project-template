4e73751df0b733d0bd68f942e4355336f94809ab
mawiav08 <mawiav008@gmail.com>
2024-07-12 20:28:19 +0300

    initial commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Home from './Home';
+import MensShoes from './MensShoes';
+import WomensShoes from './WomensShoes';
+import KidsShoes from './KidsShoes';
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/mens-shoes" component={MensShoes} />
+          <Route path="/womens-shoes" component={WomensShoes} />
+          <Route path="/kids-shoes" component={KidsShoes} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
